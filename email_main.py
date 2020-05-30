b8dcef78df6f31035ef56f6f61e76aeaadfa0f07
munachimso-victor <61572898+munachimso-victor@users.noreply.github.com>
2020-05-30 01:31:09 -0500

    Add files via upload

---
+++ email_main.py
@@ -0,0 +1,62 @@
+import functions
+import sys
+
+
+
+def email_main():
+    print(
+        """
+        Local Portion:
+        The local-part of the email address may be unquoted or may be enclosed in quotation marks.
+        The maximum total length of the local-part of an email address is 64 octets.
+
+        If unquoted, it may use any of these ASCII characters:
+        =>uppercase and lowercase Latin letters A to Z and a to z;
+        =>digits 0 to 9;
+        =>printable characters !#$%&'*+-/=?^_`{|}~;
+        =>dot , provided that it is not the first or last character and provided also t
+        hat it does not appear consecutively (e.g., John..Doe@example.com is not allowed)
+        If in a quoted string:
+        =>space and special characters "(),:;<>@[\] are allowed with restrictions (they are only allowed inside a quoted string,
+        as described in the paragraph below, and in addition, a backslash or double-quote must be preceded by a backslash);
+
+        Domain portion:
+        The domain name part of an email address has to conform to strict guidelines: it must match the requirements for a hostname,
+        a list of dot-separated DNS labels, each label being limited to a length of 63 characters and consisting of:
+        =>uppercase and lowercase Latin letters A to Z and a to z;
+        =>digits 0 to 9, provided that top-level domain names are not all-numeric;
+        =>hyphen -, provided that it is not the first or last character.
+
+        *The details instructions above were gotten from https://en.wikipedia.org/wiki/Email_address. Visit the page for more info on the syntax
+        *This program does not accept comments in the local or domain section and IP address literals
+        * The maximum email length is based on the RFC 5321 and the erracta submitted against the RFC 3696. This erracta specifies that
+        the maximum number of chaaracters is 254 due to the reverse and forward path specification 
+        """
+    )
+    print("\nThe fromat for a valid Email Address is local@domain E.g. dave@google.com")
+
+    local = input("Type your Email Address LOCAL Portion: ")
+    domain = input("Type your Email Address DOMAIN Portion: ")
+    local_length = len(local)
+    domain_length = len(domain)
+
+    if local_length + domain_length > 253:
+        return "Invalid Address: Too Long"
+
+
+    email_address = f"{local}@{domain}"
+    a = functions.is_local_valid(local, local_length)
+    b = functions.is_domain_valid(local, domain, local_length, domain_length)
+
+    if a and b:
+        if functions.dns_lookup_domain(domain):
+            return f"\nThe email address {email_address} is VALID, and the domain exists!"
+        else:
+            return f"\nThe email address {email_address} is VALID, but the domain does not exist"
+
+    else:
+        return f"\nTHE EMAIL ADDRESS {email_address} is INVALID"
+
+
+if __name__ == "__main__":
+    print(email_main())
